ae6498fd20cd14e5c8b066e7d878ee289d21bf91
Guhan M <116095658+guhan0000@users.noreply.github.com>
2024-07-30 20:02:04 +0530

    Add files via upload

---
+++ datatypes.java
@@ -0,0 +1,29 @@
+
+public class datatypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		long num=1000000000023L;
+		long l2=-1000000000023L;
+		byte num2=-127;
+		short s1=30000;
+		short s2=-30000;
+		float f1=30.12345411f;
+		double d1=3423097407327.73207423;
+		char c='#';
+		float n=10;
+		n=n/3;
+		int n2=10;
+		n2=n2%2;
+		System.out.println(n2);
+		System.out.printf("%.2f",n);
+		String song="nennje nenje nee yenga naanum ange en jeeven ange";
+		System.out.println(song);
+		int a=2;
+		a=(int) Math.pow(a,5);
+		System.out.println(a);
+		
+
+	}
+
+}
