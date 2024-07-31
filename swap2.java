5f1ff62dd398f17e3a6c51fe0a3f8ac43d5ee0bd
Guhan M <116095658+guhan0000@users.noreply.github.com>
2024-07-31 20:17:12 +0530

    Add files via upload

---
+++ swap2.java
@@ -0,0 +1,15 @@
+
+public class swap2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=5;
+		int temp;
+		temp=a;
+		a=b;
+		b=temp;
+		System.out.print(a+" "+b);
+	}
+
+}
