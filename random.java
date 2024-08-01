6ab30b1336274ac1163979d87ef6d65ad590b298
Guhan M <116095658+guhan0000@users.noreply.github.com>
2024-08-01 20:56:09 +0530

    Add files via upload

---
+++ random.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class random {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Random random = new Random();
+		float x= random.nextFloat();
+		//int a=(int) Math.ceil(random.nextInt(6));
+		int a=random.nextInt(6)+1;
+		boolean b=random.nextBoolean();
+		
+		
+		
+		System.out.println(b);
+		
+
+	}
+
+}
