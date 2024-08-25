0595096ac1916058588194f9aec119eacd8badd7
Guhan M <116095658+guhan0000@users.noreply.github.com>
2024-08-25 10:50:38 +0530

    Add files via upload

---
+++ exfinal.java
@@ -0,0 +1,11 @@
+
+public class exfinal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final double PI=3.14;
+		int n1 =(int) Math.round(PI);
+		System.out.println(n1);
+	}
+
+}
