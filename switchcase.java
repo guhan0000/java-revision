5807ac2de9a37c215f76bb6ebbe5c7d7e6e30d54
Guhan M <116095658+guhan0000@users.noreply.github.com>
2024-08-25 10:49:40 +0530

    Add files via upload

---
+++ switchcase.java
@@ -0,0 +1,38 @@
+
+public class switchcase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String day="guhan";
+		switch(day)
+		{
+		case "monday":
+			System.out.println("it is monday");
+			break;
+		case "tuesday":
+			System.out.println("it is tuesday");
+			break;
+		case "wednesday":
+			System.out.println("it is wednesday");
+		break;
+		case "thursday":
+			System.out.println("it is thursday");
+			break;
+		case "friday":
+			System.out.println("it is friday");
+			break;
+		case "saturday":
+			System.out.println("it is saturday");
+			break;
+		case "sunday":
+			System.out.println("it is sunday");
+			break;
+			default:
+				System.out.println(day+" is not a day");
+				break;
+		
+		}
+
+	}
+
+}
